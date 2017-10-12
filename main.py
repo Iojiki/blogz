815393f7b6876c85c1d469aeb154db1e22803823
Iojiki <eeohjeeki@gmail.com>
2017-10-12 13:14:06 -0500

    got code functionality working with fixed body

--- main.py
+++ main.py
@@ -26,13 +26,39 @@ def index():
 
 @app.route('/blog')
 def blog():
+    entries = Blog.query.order_by(Blog.id.desc()).all()
+    return render_template('blog.html', title = 'Blog', entries = entries)
 
-    return render_template('blog.html', title = 'Blog')
-
-@app.route('/newpost')
+@app.route('/newpost', methods = ['GET','POST'])
 def newpost():
+    if request.method == "GET":
+        return render_template("newpost.html", title = 'Newpost')
 
-    return render_template('newpost.html', title = 'Newpost')
+    elif request.method == "POST":
+        name = request.form["name"]
+        body = request.form["body"]
+        error = False
+        error_title = ''
+        error_body = ''
+        if name == "":
+            error = True
+            error_title = "Please include a title"
+        if body == "":
+            error = True
+            error_body = "Please enter text in your post"
+        if error == True:
+            return render_template("newpost.html", title = 'Newpost', error_title = error_title, error_body = error_body)
+        else:
+            blog_post = Blog(name, body)
+            db.session.add(blog_post)
+            db.session.commit()
+            id = blog_post.id
+            return redirect("/submit?id={}".format(id))
+@app.route('/submit')
+def submit():
+    id = request.args.get("id")
+    blog_submit = Blog.query.filter_by(id = id).first()
+    return render_template("submit.html", blog_submit = blog_submit) 
 
 if __name__ == "__main__":
     app.run()
